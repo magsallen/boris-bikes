4009652626890193212298ea4ae6067e1448775d
Hanna <hannaeberhoefer@yahoo.com>
2016-04-12 16:53:26 +0100

    Challenge 16 incomplete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -40,7 +40,7 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when full' do
-      20.times {subject.dock(Bike.new)}
+      @DEFAULT_CAPACITY.to_i.times {subject.dock(Bike.new)}
       expect {subject.dock Bike.new}.to raise_error 'Docking station full'
     end
   end
