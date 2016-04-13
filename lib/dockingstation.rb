f1e155b65ac35765b37a4d9197fea51d1d2390f9
Noah Pollock <knowerlittle@Noahs-MacBook-Pro.local>
2016-04-13 16:11:01 +0100

    Day 2 Maggie

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,5 +1,21 @@
+require_relative 'bike'
+
 class DockingStation
+attr_reader :bike
+#attr_accessor :array_of_bikes
+
+	# def initialize
+	# 	@array_of_bikes = []
+	# end
+
 	def release_bike
-		Bike.new
+		raise "Station empty" unless @bike
+	#	Bike.new
+#		end
 	end
+
+	def dock(bike)
+		bike = @bike
+	end
+
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,12 +1,33 @@
 require 'dockingstation'
 
 describe DockingStation do
+	DockingStation.new
+
 	it "releases a bike method" do
 	expect(subject).to respond_to(:release_bike)
 	end
 
+	it "raises an error if empty" do
+	#if subject.array_of_bikes.empty?
+	expect{subject.release_bike}.to raise_error("Station empty")
+	end
+
 	it "creates a new bike" do
-		bike = subject.release_bike
-		expect(bike).to be_working
+		bike = Bike.new
+		puts "yes, new bike has been created :)"
+	end
+
+	it { is_expected.to respond_to(:bike)}
+	it { should respond_to(:dock).with(1).argument }
+
+	it 'expect the bike to be working' do
+		# station = DockingStation.new
+		bike = Bike.new
+		expect(bike.working?).to be true
+	end
+
+	it 'should be able to dock a bike' do
+		bike = Bike.new
+		expect(subject.dock(bike)).to eq(bike)
 	end
 end
