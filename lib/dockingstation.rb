fa00529ef12699926dda552b7d9f59729463c4de
pixelandpage <mags@pixelandpage.co.uk>
2016-04-14 10:30:20 +0100

    day three challenge 15 adding full and empty methods

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,20 +2,30 @@ require_relative 'bike'
 
 class DockingStation
 attr_reader :bike
-attr_accessor :array_of_bikes
+#attr_accessor :array_of_bikes
 
 	def initialize
 	 	@array_of_bikes = []
 	end
 
 	def release_bike
-		raise "Station empty" if @array_of_bikes.empty?
+		raise "Station empty" if empty?
 		@array_of_bikes.pop
 	end
 
 	def dock(bike)
-		raise "Station full" if @array_of_bikes.length >= 20
+		raise "Station full" if full?
 		@array_of_bikes << bike
 	end
 
+
+	private
+
+	def full?
+	  @array_of_bikes.length >= 20
+	end
+
+	def empty?
+	  @array_of_bikes.length <= 0
+	end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,38 +1,31 @@
 require 'dockingstation'
 
-#SUBJECT IS AN INTANCE OF WHAT YOU DESCRIBE
+#SUBJECT IS AN INSTANCE OF WHAT YOU DESCRIBE
 describe DockingStation do
 	bike = Bike.new
 
 	it "releases a bike method" do
-	expect(subject).to respond_to(:release_bike)
+		expect(subject).to respond_to(:release_bike)
 	end
 
-	# it "raises an error if empty" do
-	# 	if subject.array_of_bikes.count == 0
-	# 		expect{subject.release_bike}.to raise_error("Station empty")
-	# 	end
-	# end
+	it "raises an error if empty" do
+		expect{subject.release_bike}.to raise_error("Station empty")
+	end
 
 	it "raises an error if full" do
-
-		#if subject.array_of_bikes.count > 20
 		subject.dock(Bike.new)
 		expect{20.times{subject.dock(Bike.new)}}.to raise_error("Station full")
 	end
 
-	it "creates a new bike" do
-		puts "yes, new bike has been created :)"
-	end
-
 	it { is_expected.to respond_to(:bike)}
+
 	it { should respond_to(:dock).with(1).argument }
 
 	it 'expect the bike to be working' do
 		expect(bike.working?).to be true
 	end
 
-	it 'should be able to dock a bike' do
+	it 'should be able to dock an instance of a bike' do
 		expect(subject.dock(bike)).to eq([bike])
 	end
 end
