ce0d997b762da8a9d644ea8a0f101c972d80588d
pixelandpage <mags@pixelandpage.co.uk>
2016-04-14 16:14:22 +0100

    challenge 20 completed: doubles

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,9 +1,6 @@
 require 'dockingstation'
-require 'bike'
 
-#SUBJECT IS AN INSTANCE OF WHAT YOU DESCRIBE
 describe DockingStation do
-	bike = Bike.new
 
 	it "releases a bike method" do
 		expect(subject).to respond_to(:release_bike)
@@ -14,19 +11,22 @@ describe DockingStation do
 	end
 
 	it "raises an error if full" do
-		subject.dock(Bike.new)
-		expect{DockingStation::DEFAULT_CAPACITY.times{subject.dock(Bike.new)}}.to raise_error("Station full")
+		subject.dock double(:bike) 
+		expect{DockingStation::DEFAULT_CAPACITY.times{subject.dock double(:bike)}}.to raise_error("Station full") 
 	end
 
 	it { is_expected.to respond_to(:bike)}
 
 	it { should respond_to(:dock).with(1).argument }
 
+	let(:bike) {double :bike}
 	it 'expect the bike to be working' do
+		allow(bike).to receive(:working?).and_return(true)
 		expect(bike.working?).to be true
 	end
 
 	it 'should be able to dock an instance of a bike' do
+		bike = double(:bike)
 		expect(subject.dock(bike)).to eq([bike])
 	end
 
@@ -39,13 +39,14 @@ describe DockingStation do
 		expect(station.capacity).to eq 10
 	end
 
-	it 'should be report a bike as broken' do
+	it 'should be able to report a bike as broken' do
+		allow(bike).to receive(:report_broken).and_return(false)
 		expect(bike.report_broken).to eq false
 	end
 
 	it 'should not release broken bikes' do
-		bike.report_broken
-	  subject.dock(bike)
+		bike = double("bike", :working? => false)
+	  	subject.dock(bike)
 		expect{subject.release_bike}.to raise_error("Can't release a broken bike")
 	end
 end
